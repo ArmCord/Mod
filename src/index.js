cb78156baa3878d15da18d703d23400946c485e8
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2023-07-26 20:44:20 +0200

    add armcord check

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { log } from "./lib";
 import { patchScreenshareQuality } from "./patches/screenshareQuality";
-import {inject} from "./patches/desktopKeybinds"
-log("Loading...")
-window.ACHelper = {
-    patchScreenshareQuality: patchScreenshareQuality
-}
-try {
-    log("Loading screenshare quality patch!")
-    //inject()
-    patchScreenshareQuality(60, 720)
-} catch (e) {
-    console.error(e)
+if (window.armcord) {
+    log("Loading...")
+    window.ACHelper = {
+        patchScreenshareQuality: patchScreenshareQuality
+    }
+    try {
+        log("Loading screenshare quality patch!")
+        //inject()
+        patchScreenshareQuality(60, 720)
+    } catch (e) {
+        console.error(e)
+    }
 }
